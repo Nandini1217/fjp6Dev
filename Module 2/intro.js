88f692beb1031804756cc045b1362f49b920fa53
Nandini1217 <nandini.solanki1217@gmail.com>
2022-03-10 00:46:06 +0530

    intro to js

---
+++ Module 2/intro.js
@@ -0,0 +1,29 @@
+
+//printing in JavaSript
+console.log("welcome guys to javascript world");
+
+console.log("pepcoders");
+
+//variables in JS
+let a=10;
+console.log(a);
+
+let c='d';
+console.log(c);
+
+let b=true;
+console.log(b);
+
+let s="Nandini";
+console.log(s);
+
+//loops
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+
+let count=10;
+while(count>0){
+    console.log(count);
+    count--;
+}
