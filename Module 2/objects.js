c6fa93f0341cfeb2f447a563815e76a9533141f3
Nandini1217 <nandini.solanki1217@gmail.com>
2022-03-15 23:09:07 +0530

    objects

---
+++ Module 2/objects.js
@@ -0,0 +1,26 @@
+//empty object
+let obj={};
+console.log(obj);
+
+//object
+let objNew={
+    "first":"Nandini",
+    "Age":25,
+    last:'Solanki'
+};
+console.log(objNew);
+
+//key can have any value
+let capAmerica={
+    Name:'Steve',
+    Age:999,
+    Friends:['Thor','Hulk','Tony'],
+    address:{
+        city:'Queens',
+        state:'DC'
+    },
+    sayHi: function(){
+        console.log("hi");
+    }
+}
+console.log(capAmerica);
