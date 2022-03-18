7636366f87869a6ba0dddbf994d2b0127a68da8f
Nandini1217 <nandini.solanki1217@gmail.com>
2022-03-18 12:01:18 +0530

    os

---
+++ Module 2/Node/os.js
@@ -0,0 +1,6 @@
+let os=require("os");
+console.log(os);
+
+console.log(os.hostname());
+console.log(os.arch()); //tells us the bit od opertaing system ie.32 or 64
+console.log(os.networkInterfaces());
