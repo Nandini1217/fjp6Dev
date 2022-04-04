3acce7ce181a986eb8151c163adda9e9f10cf433
Nandini1217 <nandini.solanki1217@gmail.com>
2022-04-04 22:22:05 +0530

    q5

--- Module2/Node/questions/q5.js
+++ Module2/Node/questions/q5.js
@@ -3,10 +3,10 @@
 let fs = require("fs");
 let path = require("path");
 
-let srcPath = path.join(__dirname,"file1.txt");
-// console.log(srcPath);
-let destPath = path.join(__dirname,"newFolder","file1.txt");
-// console.log(destPath);
+let srcPath=path.join(__dirname,"file2.txt");
+
+let destPath=path.join(__dirname,"NewFolder","file1.txt");
+
 fs.copyFileSync(srcPath,destPath);
 
 fs.unlinkSync(srcPath);
