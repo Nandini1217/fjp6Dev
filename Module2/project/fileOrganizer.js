7bb4abc2a02b7aedc10b4dce9122fab8bd676648
Nandini1217 <nandini.solanki1217@gmail.com>
2022-04-06 23:55:21 +0530

    project

--- Module2/project/fileOrganizer.js
+++ Module2/project/fileOrganizer.js
@@ -1,3 +1,17 @@
 // how to take input
-let input=process.argv[2];
-console.log(input);
+// let input=process.argv[2];
+// console.log(input);
+
+let fs=require("fs");
+
+let folderPath=process.argv[2];
+//console.log(folderPath);
+
+let foldExists=fs.existsSync(folderPath)
+
+if(foldExists){
+    //read folder
+console.log("path is valid");
+}else{
+    console.log("please enter a valid path")
+}
